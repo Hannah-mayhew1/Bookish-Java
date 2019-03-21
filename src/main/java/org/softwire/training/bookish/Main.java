eaa2dbac4c715f602972f6284be77ff38b66bc06
Hannah.Mayhew <hannah.mayhew@HannahMayhews-MacBook-Pro.local>
2019-03-21 17:15:23 +0000

    Choose a category

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -5,6 +5,7 @@ import org.softwire.training.bookish.models.database.Book;
 
 import java.sql.*;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Main {
@@ -16,41 +17,45 @@ public class Main {
         String password = "password";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
-        jdbcMethod(connectionString);
-        jdbiMethod(connectionString);
+        bookTitlesBasedOnCategory(connectionString);
     }
 
-    private static void jdbcMethod(String connectionString) throws SQLException {
-        System.out.println("JDBC method...");
-
-        // TODO: print out the details of all the books (using JDBC)
-        // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
-
-        Connection connection = DriverManager.getConnection(connectionString);
-        Statement stmt = null;
-        String query = "SELECT * FROM bookish.books ORDER BY book_title ASC";
-
-        stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-            String bookTitle = rs.getString("book_title");
-            System.out.println(bookTitle);
-        }
-
-
-    }
-
-    private static void jdbiMethod(String connectionString) {
-        System.out.println("\nJDBI method...");
-
-        // TODO: print out the details of all the books (using JDBI)
-        // See this page for details: http://jdbi.org
-        // Use the "Book" class that we've created for you (in the models.database folder)
+    private static void bookTitlesInAscOrder(String connectionString) {
 
         Jdbi jdbi = Jdbi.create(connectionString);
         String query = "SELECT * FROM bookish.books ORDER BY book_title ASC";
         List<Book> books = jdbi.withHandle(handle -> {
             return handle.createQuery(query)
+                    .mapToBean(Book.class)
+                    .list();
+        });
+
+        for (Book book : books) {
+            System.out.println(book.getBookTitle());
+        }
+    }
+
+    private static void bookTitlesBasedOnCategory(String connectionString) {
+        Jdbi jdbi = Jdbi.create(connectionString);
+
+        String categoriesQuery = "SELECT DISTINCT category FROM books";
+        String booksQuery = "SELECT * FROM bookish.books WHERE category = :category";
+
+        List<Book> books = jdbi.withHandle(handle -> {
+
+            List<String> categories = handle.createQuery(categoriesQuery)
+                    .mapTo(String.class)
+                    .list();
+
+            Scanner categoryInputScanner = new Scanner(System.in);
+            for (String category : categories) {
+                System.out.print(category + " ");
+            }
+            System.out.println("\nWhich category would you like to search for?");
+            String categoryInput = categoryInputScanner.next();
+
+            return handle.createQuery(booksQuery)
+                    .bind("category", categoryInput)
                     .mapToBean(Book.class)
                     .list();
         });
