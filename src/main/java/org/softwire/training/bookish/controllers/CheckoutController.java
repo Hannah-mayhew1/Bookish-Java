6df4e4a3d44200994bc0c7de0674aae5d840bdd4
Anna Allen <Anna.kettlewell@yahoo.co.uk>
2019-03-26 14:54:09 +0000

    checkPage HTML

--- src/main/java/org/softwire/training/bookish/controllers/CheckoutController.java
+++ src/main/java/org/softwire/training/bookish/controllers/CheckoutController.java
@@ -3,41 +3,49 @@ package org.softwire.training.bookish.controllers;
 import org.softwire.training.bookish.models.database.Book;
 import org.softwire.training.bookish.models.database.Checkout;
 import org.softwire.training.bookish.models.database.Member;
-import org.softwire.training.bookish.models.database.Technology;
-import org.softwire.training.bookish.models.page.AboutPageModel;
 import org.softwire.training.bookish.models.page.CatalogueModel;
 import org.softwire.training.bookish.models.page.CheckoutModel;
 import org.softwire.training.bookish.models.page.MembersModel;
 import org.softwire.training.bookish.services.BookService;
 import org.softwire.training.bookish.services.CheckoutService;
 import org.softwire.training.bookish.services.MemberService;
-import org.softwire.training.bookish.services.TechnologyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
+import org.softwire.training.bookish.services.BookService;
 
 @Controller
 @RequestMapping("/checkout")
 public class CheckoutController {
+    private final MemberService memberService;
     private final CheckoutService checkoutService;
+    private final BookService bookService;
 
     @Autowired
-    public CheckoutController(CheckoutService checkoutService) { this.checkoutService = checkoutService; }
+    public CheckoutController(MemberService memberService, CheckoutService checkoutService, BookService bookService) {
+        this.memberService = memberService;
+        this.checkoutService = checkoutService;
+        this.bookService = bookService;
+    }
 
     @RequestMapping("")
     ModelAndView booksOnLoan() {
 
         List<Checkout> allBooksOnLoan = checkoutService.getAllBooksOnLoan();
-
         CheckoutModel checkoutModel = new CheckoutModel();
         checkoutModel.setBooksOnLoan(allBooksOnLoan);
 
-        return new ModelAndView("booksOnLoan", "model", checkoutModel);
+        List<Member> allMembers = memberService.getAllMembers();
+        MembersModel membersModel = new MembersModel();
+        membersModel.setMembers(allMembers);
+
+        List<Book> allBooks = bookService.getAllBooks();
+        CatalogueModel catalogueModel = new CatalogueModel();
+        catalogueModel.setBooks(allBooks);
+
+        return new ModelAndView("checkoutPage", "model", checkoutModel);
     }
 }
